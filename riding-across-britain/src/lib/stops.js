ab5b5f735aebc4b185b4468871b4c66dcef61eb2
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2021-01-02 22:08:50 -0600

    Add list of stops

---
+++ riding-across-britain/src/lib/stops.js
@@ -0,0 +1,66 @@
+// src: https://www.rideacrossbritain.com/app/uploads/2019/06/national-map-stages-01.jpeg
+
+const stops = [
+    {
+        city: "Land's End",
+        page: "https://en.wikipedia.org/wiki/Land%27s_End",
+        lat: 50.0686111,
+        lon: -5.7161111,
+    },
+    {
+        city: "Okehampton",
+        page: "https://en.wikipedia.org/wiki/Okehampton",
+        lat: 50.7388,
+        lon: -4.0041,
+    },
+    {
+        city: "Bath", // Bath, Somerset
+        page: "https://en.wikipedia.org/wiki/Bath,_Somerset",
+        lat: 51.38,
+        lon: -2.36,
+    },
+    {
+        city: "Ludlow",
+        page: "https://en.wikipedia.org/wiki/Ludlow",
+        lat: 52.368,
+        lon: -2.718,
+    },
+    {
+        city: "Haydock",
+        page: "https://en.wikipedia.org/wiki/Haydock",
+        lat: 53.4678,
+        lon: -2.6609,
+    },
+    {
+        city: "Carlisle",
+        page: "https://en.wikipedia.org/wiki/Carlisle",
+        lat: 54.891,
+        lon: -2.944,
+    },
+    {
+        city: "Hopetoun House",
+        page: "https://en.wikipedia.org/wiki/Hopetoun_House",
+        lat: 55.9954,
+        lon: -3.4629,
+    },
+    {
+        city: "Strathdon",
+        page: "https://en.wikipedia.org/wiki/Strathdon",
+        lat: 57.199569,
+        lon: -3.075739,
+    },
+    {
+        city: "Kyle of Sutherland",
+        page: "https://en.wikipedia.org/wiki/Kyle_of_Sutherland",
+        lat: 57.925,
+        lon: -4.4,
+    },
+    {
+        city: "John o' Groats",
+        page: "https://en.wikipedia.org/wiki/John_o%27_Groats",
+        lat: 58.64,
+        lon: -3.07
+    }
+];
+
+module.exports = stops;
