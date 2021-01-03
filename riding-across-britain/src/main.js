7e0dbc95c41cddc1bc29e53dff8472b3ca57cf3e
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2021-01-03 15:34:27 -0600

    Fix code style

--- riding-across-britain/src/main.js
+++ riding-across-britain/src/main.js
@@ -1,47 +1,64 @@
 const styles = require("./static/styles.css");
 const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 
-const { coordinates, bounds } = require("./lib/stops");
-const R = require("ramda");
+const { bounds } = require("./lib/stops");
 
 const data = require("./lib/route.json");
 
 function drawRoute(map)
 {
-    map.on('load', function ()
+    map.on("load", () =>
     {
-        map.addSource('route', data);
+        map.addSource("route", data);
         map.addLayer({
-            'id': 'route',
-            'type': 'line',
-            'source': 'route',
-            'layout':
+            id: "route",
+            type: "line",
+            source: "route",
+            layout:
             {
-                'line-join': 'round',
-                'line-cap': 'round'
+                "line-join": "round",
+                "line-cap": "round"
             },
-            'paint':
+            paint:
             {
-                'line-color': '#fff',
-                'line-width': 5,
-                'line-opacity': 0.75
+                "line-color": "#fff",
+                "line-width": 5,
+                "line-opacity": 0.75
+            }
+        });
+
+        map.addLayer({
+            id: "points",
+            type: "symbol",
+            source: "route",
+            layout:
+            {
+                // get the title name from the source's "title" property
+                "text-field": ["get", "title"],
+                "text-font": [
+                    "Open Sans Semibold",
+                    "Arial Unicode MS Bold"
+                ],
+                "text-offset": [0, 1.25],
+                "text-anchor": "center"
             }
         });
     });
 }
 
-(() => {
+(() =>
+{
     const wrapper = document.createElement("div");
     wrapper.id = "map";
     document.body.appendChild(wrapper);
     if (wrapper)
     {
-        mapboxgl.accessToken = 'pk.eyJ1IjoicG91d2Vya2VyayIsImEiOiJjaXFqZWxkZGIwOXRoZnRuZTJ0M2hocTVrIn0.p-TeDs92ZEJ5QtaHtYH2Og';
+        mapboxgl.accessToken = "pk.eyJ1IjoicG91d2Vya2VyayIsImEiOiJjaXFqZWxkZGIwOXRoZnRuZTJ0M2hocTVrIn0.p-TeDs92ZEJ5QtaHtYH2Og";
 
         const map = new mapboxgl.Map({
             container: wrapper.id,
             bounds: bounds,
-            style: 'mapbox://styles/mapbox/streets-v11'
+            style: "mapbox://styles/mapbox/streets-v11"
         });
 
         drawRoute(map);
